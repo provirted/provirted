88ee89cf6463565e9731192c7d4b402e81472099
Joe Huss <detain@interserver.net>
2021-12-03 17:45:01 -0500

    exit code handling imrpovement

--- app/Command/TestCommand.php
+++ app/Command/TestCommand.php
@@ -28,7 +28,6 @@ class TestCommand extends Command {
 
 	public function execute($vzid, $password = '') {
 		$this->getLogger()->writeln('Running Tests on '.$vzid);
-		$this->getLogger()->newline();
 		//$logger = new ActionLogger(fopen('php://stderr','w'), new Formatter);
 		$logger = new ActionLogger(fopen('php://stdout','w'), new Formatter);
 		$logAction = $logger->newAction('Virtualization Installed');
@@ -38,7 +37,7 @@ class TestCommand extends Command {
 			$logAction->setStatus('error');
 			$this->getLogger()->error("This machine does not appear to have any virtualization setup installed.");
 			$this->getLogger()->error("Check the help to see how to prepare a virtualization environment.");
-			return 1;
+			exit(1); // return 1;
 		}
 		$logAction->setStatus('success');
 		$logAction->done();
@@ -47,7 +46,7 @@ class TestCommand extends Command {
 		if (!Vps::vpsExists($vzid)) {
 			$logAction->setStatus('error');
 			$this->getLogger()->error("The VPS '{$vzid}' you specified does not appear to exist, check the name and try again.");
-			return 1;
+			exit(1); // return 1;
 		}
 		$logAction->setStatus('success');
 		$logAction->done();
@@ -56,7 +55,7 @@ class TestCommand extends Command {
 		if (!Vps::isVpsRunning($vzid)) {
 			$logAction->setStatus('error');
 			$this->getLogger()->error("The VPS '{$vzid}' you specified appears to be stopped.");
-			return 1;
+			exit(1); // return 1;
 		}
 		$logAction->setStatus('success');
 		$logAction->done();
@@ -66,7 +65,7 @@ class TestCommand extends Command {
 		if (!array_key_exists($vzid, $hosts)) {
 			$logAction->setStatus('error');
 			$this->getLogger()->error("Dhcpd does not appear to have {$vzid} among the configured hosts (".implode(', ', array_keys($hosts)).")");
-			return 1;
+			exit(1); // return 1;
 		}
 		$logAction->setStatus('success');
 		$logAction->done();
@@ -79,12 +78,12 @@ class TestCommand extends Command {
 				if (!in_array($data['ip'], $ips)) {
 					$logAction->setStatus('error');
 					$this->getLogger()->error("The VPS does not appear to have the right ip in DHCP ({$data['ip']} not in ".implode(', ', $ips).")");
-					return 1;
+					exit(1); // return 1;
 				}
 				if ($data['mac'] != $mac) {
 					$logAction->setStatus('error');
 					$this->getLogger()->error("The VPS does not appear to have the right mac in DHCP ({$data['mac']} != {$mac})");
-					return 1;
+					exit(1); // return 1;
 				}
 				$ip = $data['ip'];
 			}
@@ -96,7 +95,7 @@ class TestCommand extends Command {
 		if (!Dhcpd::isRunning()) {
 			$logAction->setStatus('error');
 			$this->getLogger()->error("Dhcpd does not appear to be running.");
-			return 1;
+			exit(1); // return 1;
 		}
 		$logAction->setStatus('success');
 		$logAction->done();
@@ -108,7 +107,7 @@ class TestCommand extends Command {
 		if (!Xinetd::isRunning()) {
 			$logAction->setStatus('error');
 			$this->getLogger()->error("XinetD does not appear to be running.");
-			return 1;
+			exit(1); // return 1;
 		}
 		$logAction->done();
 
@@ -118,7 +117,7 @@ class TestCommand extends Command {
 		if (trim(Vps::runCommand('ping -c 1 '.$ip.' -q -n >/dev/null && echo yes')) != 'yes') {
 			$logAction->setStatus('error');
 			$this->getLogger()->error("Did not respond to ping.");
-			return 1;
+			exit(1); // return 1;
 		}
 		$logAction->done();
 
@@ -128,7 +127,7 @@ class TestCommand extends Command {
 		if (!$con) {
 			$logAction->setStatus('error');
 			$this->getLogger()->error('SSH Connection failed to "'.$ip.'": '.var_export($con,true));
-			return 1;
+			exit(1); // return 1;
 		}
 		$logAction->done();
 
@@ -137,7 +136,7 @@ class TestCommand extends Command {
 		if (!@ssh2_auth_password($con, 'root', $password)) {
 			$logAction->setStatus('error');
 			$this->getLogger()->error('SSH Password Authentication failed using "'.$password.'": '.var_export($con,true));
-			return 1;
+			exit(1); // return 1;
 		}
 	    $logAction->done();
 
@@ -150,7 +149,7 @@ class TestCommand extends Command {
 		if ($response != 'yes') {
 			$logAction->setStatus('error');
 			$this->getLogger()->error('Pinging 1.1.1.1 on VPS via SSH failed and returned: "'.$response.'"');
-			return 1;
+			exit(1); // return 1;
 		}
 	    $logAction->done();
 		if ($con) {
