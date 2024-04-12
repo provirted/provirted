0b481909c45b7ab0523d13bcf646c482c7a9ba88
Joe Huss <detain@interserver.net>
2024-04-12 14:45:48 -0400

    fix

--- app/Command/CreateCommand.php
+++ app/Command/CreateCommand.php
@@ -90,7 +90,7 @@ HELP;
         $ipv6Range = array_key_exists('ipv6-range', $opts->keys) ? $opts->keys['ipv6-range']->value : false;
         $ioLimit = $useAll === false && array_key_exists('io-limit', $opts->keys) ? $opts->keys['io-limit']->value : false;
         $iopsLimit = $useAll === false && array_key_exists('iops-limit', $opts->keys) ? $opts->keys['iops-limit']->value : false;
-        if ($useAll == true && count(Vps::getAllVps()) > 0) {
+        if ($useAll == true && trim(`virsh list --all|grep qs`) != '') {
             Vps::getLogger()->error("There is already a VPS on this system so it cannot create one that uses all resources.");
             return 1;
         }
